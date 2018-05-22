92ef45ab80c3222e087084b439ae469ba82a41cf
73tong <31788140+673tong@users.noreply.github.com>
2018-05-22 23:14:16 +0800

    文件工具模块复制文件夹下面的指定结尾文件

--- kisen/src/main/java/top/tong73/FileUtil.java
+++ kisen/src/main/java/top/tong73/FileUtil.java
@@ -1,46 +1,94 @@
 package top.tong73;
 
-import java.io.File;
+import java.io.*;
 
 public class FileUtil {
 
 	public static void main(String[] args) {
-        File file = new File("D:\\MyFileTest");
+        //给出要复制的源文件夹与目标文件夹
+        File src = new File("H:\\北京传智26期资料-无视频版");
+        File dest = new File("D:\\copy");
 
-		// 遍历文件夹
-		listDir(file);
+        System.out.println("开始复制");
+        copyDIR2DIR(src, dest);
+        System.out.println("复制结束");
 	}
-	
-	/**
-	 * 
-	 * @param file 要遍历的文件夹
-	 */
-	public static void listDir(File file) {
-		//System.out.println(file + "*");
-		
-		// 列出文件夹里面的所有内容
-		File[] files = file.listFiles();
-		
-		// 遍历获取每个File对象
-		for (File f : files) {
-			// 文件夹。接着在遍历这个文件夹
-			if (f.isDirectory()) {
-				listDir(f);
-			} else if(f.isFile()){	// 文件
-				System.out.println(f);//打印出文件的路径
-                //复制.doc文件到指定目录
-                if (f.getName().endsWith(".doc")) {
-					System.out.println(".doc:"+f);
-                    File file2 = new File("C:\\MyFileTest\\copy\\" + f.getName());
-                    boolean renameFlag = f.renameTo(file2);
-					if(renameFlag){
-                        System.out.println("复制"+f+"文件成功！");
-                        return;
-                    }
 
+    /*
+     * 将源文件夹src复制到目标文件夹dest
+     */
+    public static void copyDIR2DIR(File src,File dest) {
+
+        //在目标文件夹中创建新的源文件夹
+        File destDir = dest;
+        //File destDir = new File(dest, src.getName());
+        //destDir.mkdirs();
+
+        //遍历源文件夹,获取当中所有的文件对象(可能是文件,也可能是文件夹)
+        File[] listFiles = src.listFiles();
+
+        //使用增强for循环
+        for (File listFile : listFiles) {
+            //判断是文件还是文件夹
+            if(listFile.isDirectory()) {
+                //如果是文件夹
+                //将该文件夹放到目标文件夹中新的源文件夹中
+                File oldDir = listFile;
+                File newDir = destDir;
+                //递归调用,复制文件夹
+                copyDIR2DIR(listFile, newDir);
+            }else if(listFile.isFile() && listFile.getName().endsWith(".doc") || listFile.getName().endsWith(".docx") ){// 以.doc结尾的文件
+                //如果是文件
+                //将该文件复制到目标文件夹中新的源文件夹中
+                File newFile = new File(destDir, listFile.getName());
+
+                //完成文件复制
+                copyFile(listFile, newFile);
+            }
+        }
+    }
+
+    /*
+     * oldFile:被复制的老文件
+     * newFile:复制成的新文件
+     *
+     * 使用最简单地一次一个字节的方式复制
+     */
+    private static void copyFile(File oldFile, File newFile) {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+
+        try {
+            //创建流对象
+            bis = new BufferedInputStream(new FileInputStream(oldFile));
+            bos = new BufferedOutputStream(new FileOutputStream(newFile));
+            //读入,写出
+            //定义变量记录每次复制的字节
+            int b;
+            //循环读取
+            while((b=bis.read())!=-1) {
+                //写出字节
+                bos.write(b);
+            }
+        }catch(IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+            }
 
-			}
-		}
-	}
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
